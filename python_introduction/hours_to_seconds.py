adddc7290ea2dc904539928a49a19870ee466c1f
Fidelis Abanum <abanumfidelis90@gmail.com>
2024-09-27 17:51:10 +0100

    made changes

--- python_introduction/hours_to_seconds.py
+++ python_introduction/hours_to_seconds.py
@@ -1,5 +1,8 @@
-hours  = 2
-minutes_in_an_hour = 60 
-seconds_in_a_minute = 60
-seconds = seconds_in_a_minute * (minutes_in_an_hour* hours)
-print(seconds)
+# Variable for the number of hours
+hours = 2
+
+# Calculate the number of seconds in the given hours
+seconds = hours * 3600
+
+# Print the result in the required format
+print(f'{hours} hour(s) is {seconds} seconds')
